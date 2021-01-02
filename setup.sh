e3cc6ba5003022ae21d6f1db14ad1c3116a47f29
dahlanyasa <mdahlany@gmail.com>
2021-01-02 07:45:13 +0000

    finalisasi

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get --purge remove linux-sound-base alsa-base alsa-utils
 sudo apt-get install linux-sound-base alsa-base alsa-utils alsa-firmware-loaders alsa-oss alsa-source alsa-tools
 sudo apt-get install dkms build-essential linux-headers-$(uname -r)
